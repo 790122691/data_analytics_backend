0e2b2445479ac89e7ead34023f7f6ac2a21ca9df
lz <993243422@qq.com>
2019-08-12 05:09:21 -0400

    1

--- echarts/stock/tests.py
+++ echarts/stock/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.test.client import RequestFactory,Client
 # Create your tests here.
+
+class TestStockDataAPI(TestCase):
+    def SetUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+
+
+    def TestGetStockHistory(self):
+        code = 'AAPL'
+        request = self.factory.get('/stock/GetStockInfo?stock_symbol=' + code)
