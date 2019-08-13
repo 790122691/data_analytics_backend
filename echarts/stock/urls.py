af5f80f2c8d7f4f0c634755acffe732715c53f07
lz <993243422@qq.com>
2019-08-12 22:28:27 -0400

    2

--- echarts/stock/urls.py
+++ echarts/stock/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     re_path(r'stock_history/', views.stock_history, name='stock_history'),
     path('', views.index, name='index'),
-    re_path(r'GetStockInfo', views.get_stock_history, name = 'test')
+    re_path(r'GetStockInfo', views.get_stock_history, name = 'test'),
 ]
