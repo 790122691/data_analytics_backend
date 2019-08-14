39423471bca895a077c0c7457a6179da26209a98
lz <993243422@qq.com>
2019-08-13 23:08:03 -0400

    2

--- echarts/user/urls.py
+++ echarts/user/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     #re_path(r'stock_history/', views.stock_history, name='stock_history'),
     path('', views.login_page, name='login_page'),
-    path('register_page', views.register_page, name='register_page'),
+    path('register_page/', views.register_page, name='register_page'),
     re_path(r'login', views.login, name='login'),
     re_path(r'register', views.register, name='register'),
     re_path(r'UpdatePortfolio/', views.UpdatePortfolio, name='UpdatePortfolio'),
