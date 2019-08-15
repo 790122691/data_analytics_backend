fc2a820227fda2aec7ed012cd05212820138cca0
lz <993243422@qq.com>
2019-08-14 21:41:47 -0400

    2

--- echarts/stock/urls.py
+++ echarts/stock/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     re_path(r'GetStockHistory', views.get_stock_history, name ='history'),
     re_path(r'GetStockInfo', views.get_stock_info, name ='info'),
     re_path(r'GetStockList', views.get_all_stock, name = 'stock_list'),
-    re_path(r'days', views.get_stock_history_by_date, name = 'days his'),
+    re_path(r'get_stock_history_by_date', views.get_stock_history_by_date, name = 'days his'),
 ]
